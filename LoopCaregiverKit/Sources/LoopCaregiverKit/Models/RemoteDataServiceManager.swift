f890130a1a851608d7d80efafce7b86291fe1df8
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2024-07-06 21:29:53 -0400

    Remove log

--- LoopCaregiverKit/Sources/LoopCaregiverKit/Models/RemoteDataServiceManager.swift
+++ LoopCaregiverKit/Sources/LoopCaregiverKit/Models/RemoteDataServiceManager.swift
@@ -51,8 +51,6 @@ public class RemoteDataServiceManager: ObservableObject {
     public func updateData() async {
         updating = true
         
-        let startDate = Date()
-        
         do {
             try await updateGlucoseData() // Not async as we want it to load quickly
             
@@ -90,8 +88,6 @@ public class RemoteDataServiceManager: ObservableObject {
         } catch {
             print("Error fetching data: \(error)")
         }
-        
-        print("Fetch time = \(Date().timeIntervalSince(startDate))")
         
         updating = false
         self.refreshCalculatedData()
